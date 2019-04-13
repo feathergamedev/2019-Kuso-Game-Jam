7d41f1eb0596406adb16385030da826e897a8440
nickyeh97 <nickyeh97@gmail.com>
2019-04-13 19:03:31 +0800

    新增Enemy行為

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public enum MoveType
 {
     MoveX,
     Jump,
-    Shadow
+    Shadow,
+    Drunk
 }
 
 public class EnemyMovement : MonoBehaviour
@@ -16,16 +18,23 @@ public class EnemyMovement : MonoBehaviour
     MoveType m_type;
     [Header("直走怪跑速")]
     public float Speed = 12f;
+
     [Header("跳跳怪跳躍時間")]
     [SerializeField]
     float jumpTime = 2.0f;
     [SerializeField]
     float jumpValue = 2.0f;
     float time;
+
     [Header("忍者分身時間與距離")]
     [SerializeField]
     float splitTime = 2.0f;
     public float min = -2 , max = 2;
+
+    [Header("酒醉位移")]
+    public float DrunkX = -2;
+    public float DrunkXm = 0.6f;
+
     private Rigidbody2D m_Rigidbody; 
     [SerializeField]
     private float m_MovementInputValue;
@@ -57,7 +66,7 @@ public class EnemyMovement : MonoBehaviour
     {
         //gameObject.AddComponent<EnemySpawn>();
         GameObject Enemy = gameObject;        
-        Instantiate(Enemy, transform.position + new Vector3(Random.Range(min, max), 0, 0), Quaternion.identity);
+        Instantiate(Enemy, transform.position + new Vector3(UnityEngine.Random.Range(min, max), 0, 0), Quaternion.identity);
     }
 
     private void Update()
@@ -78,6 +87,22 @@ public class EnemyMovement : MonoBehaviour
         {
             Move();
         }
+        if(m_type == MoveType.Drunk)
+        {
+            time += Time.fixedDeltaTime;
+            if (time > 1)
+            {
+                DrunkMove();
+                time = 0;
+            }
+        }
+    }
+
+    private void DrunkMove()
+    {
+        m_Rigidbody.DOMoveX(transform.position.x + DrunkX, 0.66f).SetEase(Ease.InBounce).OnComplete(()=> { m_Rigidbody.DOMoveX(transform.position.x + DrunkXm, 0.33f).SetEase(Ease.OutBounce).onComplete(); });
+        
+        //m_Rigidbody.DOMoveX(-0.5f, 1).SetEase(Ease.InCirc);
     }
 
     private void Move()

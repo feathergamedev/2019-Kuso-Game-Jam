4c11a78ef7e61fd7f4aeae7ebc871ad880d51a35
nickyeh97 <nickyeh97@gmail.com>
2019-04-13 22:16:24 +0800

    ALL speed up

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -38,7 +38,6 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody2D m_Rigidbody; 
     [SerializeField]
     private float m_MovementInputValue;
-
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
@@ -50,6 +49,11 @@ public class EnemyMovement : MonoBehaviour
         //DOJump();
         if(m_type == MoveType.Shadow)
         Invoke("ShadowSplit", splitTime);
+
+        Speed += LevelManager.instance.cur_level * 1.2f;
+        jumpValue += LevelManager.instance.cur_level * 1.2f;
+        DrunkX -= LevelManager.instance.cur_level * 0.6f;
+        DrunkXm += LevelManager.instance.cur_level * 0.2f;
     }
 
     public void MovementInput(float forwardBackward)

6a542c6148fcc9ed13932a53b71e1ea0361a2039
Wolvz <peng86david@gmail.com>
2019-04-13 16:33:11 +0800

    Scrolling background & audio manager

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    [SerializeField]
+    Transform bg01;
+
+    [SerializeField]
+    Transform bg02;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bg01.position -= new Vector3(0.1f,0,0);
+        if(bg01.position.x <= -19.5){
+            bg01.position = new Vector3(19.5f,0,0);
+        }
+        bg02.position -= new Vector3(0.1f,0,0);
+        if(bg02.position.x <= -19.5){
+            bg02.position = new Vector3(19.5f,0,0);
+        }
+    }
+}

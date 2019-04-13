b617fa81e54537c8e68292c2ae983391fdcb6f36
Wolvz <peng86david@gmail.com>
2019-04-13 15:50:56 +0800

    Generator done

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] prefabs = new GameObject[1];
+
+    public int randMin = 0;
+    public int randMax = 0;
+
+    public float sec = 0f;
+    private int rand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawn(){
+        while(true){
+            if(prefabs != null && randMax <= prefabs.Length -1){
+                rand = Random.Range(randMin,randMax);
+                Instantiate(prefabs[rand]);
+
+                yield return new WaitForSeconds(sec);
+            }
+
+            yield return null;
+        }
+    }
+}

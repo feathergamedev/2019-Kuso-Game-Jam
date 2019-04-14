b7a39c7325206b9e57edc6fea7e50218ab1de311
Shijung <1102137217@gm.kuas.edu.tw>
2019-04-14 10:19:25 +0800

    game ui

---
+++ Assets/Scripts/PlayerUIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class PlayerUIManager : MonoBehaviour
+{
+    public Image nextPullOutSprite;
+    public Image currentPullOutSprite;
+
+    Vector3 targetPos;
+    private void Start()
+    {
+        targetPos = nextPullOutSprite.transform.position;
+    }
+#if UNITY_EDITOR 
+    private void Update()
+    {
+         if (Input.GetMouseButtonDown(0))
+         {
+           UpdatePullOutThing(nextPullOutSprite.sprite);
+         }
+    }
+#endif
+    // Start is called before the first frame update
+    public void UpdatePullOutThing(Sprite next)
+    {
+        currentPullOutSprite.sprite = nextPullOutSprite.sprite;
+        nextPullOutSprite.sprite = next;
+        //DOTween.Play(nextPullOutSprite.gameObject);
+        DOTween.Restart(nextPullOutSprite.gameObject);
+    }
+
+    public void ChangeSprite()
+    {
+        Sprite tmpsp = currentPullOutSprite.sprite;
+        currentPullOutSprite.sprite = nextPullOutSprite.sprite;
+        nextPullOutSprite.sprite = tmpsp;
+    }
+
+    
+}

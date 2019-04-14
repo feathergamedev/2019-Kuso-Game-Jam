babffa089b8848f52731ed55c643a279f3ea15be
Bogay <pojay11523@gmail.com>
2019-04-14 14:28:35 +0800

    raining memes

---
+++ Assets/Scripts/FlyingDoge.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingDoge : MonoBehaviour
+{
+    public FlyingDogeGenerator generator;
+    public List<Sprite> memes = new List<Sprite>();
+    public float upperBound;
+    public float lowerBound;
+    public float rightBound;
+    public float leftBound;
+    public float speed;
+    public bool canMove;
+
+    private Transform self;
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = GetComponentInChildren<SpriteRenderer>();
+        self = transform;
+        canMove = false;
+        reset();
+    }
+
+    private void Update()
+    {
+        if(canMove) Move();
+    }
+
+    public void startFly()
+    {
+        // pre-process
+    }
+
+    public void Move()
+    {
+        self.position += Vector3.down * (speed * Time.deltaTime);
+
+        if(self.position.y < lowerBound)
+        {
+            generator.arrive(this);
+        }
+    }
+
+    public void reset()
+    {
+        sr.sprite = memes[Random.Range(0, memes.Count)];
+        self.position = new Vector3(Random.Range(leftBound, rightBound), upperBound, 0);
+    }
+}
---
+++ Assets/Scripts/FlyingDogeGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingDogeGenerator : MonoBehaviour
+{
+    public GameObject doge;
+    public int dogeCountLimit;
+    public float coolDown;
+    public float upperBound;
+    public float lowerBound;
+    public float leftBound;
+    public float RightBound;
+    public float prob;
+
+    private Queue<GameObject> dogePool = new Queue<GameObject>();
+    private int dogeCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 initPos = new Vector3(RightBound, upperBound, 0);
+
+        for(int i=0 ; i<dogeCountLimit ; i++)
+        {
+            GameObject newBornDogeObject = Instantiate(doge, initPos, Quaternion.identity);
+            FlyingDoge theDoge = newBornDogeObject.GetComponent<FlyingDoge>();
+            theDoge.generator = this;
+            dogePool.Enqueue(newBornDogeObject);
+        }
+
+        StartCoroutine(generateDoge());
+    }
+
+    public void arrive(FlyingDoge theDoge)
+    {
+        theDoge.canMove = false;
+        theDoge.reset();
+
+        dogePool.Enqueue(theDoge.gameObject);
+        dogeCount--;
+    }
+
+    private IEnumerator generateDoge()
+    {
+        while(true)
+        {
+            // gen
+            if(dogeCount < dogeCountLimit && Random.Range(0f, 1f) < prob)
+            {
+                FlyingDoge nextDoge = dogePool.Dequeue().GetComponent<FlyingDoge>();
+                nextDoge.canMove = true;
+                nextDoge.reset();
+                nextDoge.startFly();
+
+                dogeCount++;
+            }
+
+            yield return new WaitForSeconds(coolDown);
+        }
+    }
+}

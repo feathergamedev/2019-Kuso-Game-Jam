594da4fc357bd0ff78ba95ff6ecc876ee28d50e6
c19940204 <c19940204@gmail.com>
2019-04-14 13:01:03 +0800

    三個特效

---
+++ Assets/Scripts/DestorySystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestorySystem : MonoBehaviour
+{
+  public float DestoryTime = 1F;
+
+  private void Start()
+  {
+    Set();
+  }
+
+  public void Set()
+  {
+    Destroy(this.gameObject, DestoryTime);
+  }
+}

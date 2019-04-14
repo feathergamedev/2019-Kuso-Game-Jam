9ec536a967ae2563d2f6624226e534acc679f074
FeatherChung <feathergamedev@gmail.com>
2019-04-14 13:48:13 +0800

    主選單debug

---
+++ Assets/Scripts/HomePageManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomePageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "Credits")
+        {
+            if (Input.GetButtonDown("Charge"))
+                Go_To_Home();
+        }
+    }
+
+    public void Go_To_Home()
+    {
+        SceneManager.LoadScene("Bogay");
+    }
+
+    public void Go_To_Level()
+    {
+        SceneManager.LoadScene("Feather");
+
+    }
+
+    public void Go_To_Credit()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
